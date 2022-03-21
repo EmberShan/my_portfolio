db0de02014f18673d1d0b0f264114e499d008177
ember2000 <zshan@email.wm.edu>
2022-03-21 13:48:49 -0400

    changed the marquee different for mobile and desktop

--- src/components/Marquee/index.js
+++ src/components/Marquee/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Marquee from "react-fast-marquee";
 // importing scss variables 
 import colors from '../Constant.scss'
+import './Marquee.scss'
 
 const CustomMarquee = () => {
     const marqueeStyle = { 
@@ -9,14 +10,13 @@ const CustomMarquee = () => {
         backgroundColor: '#FDF0D5', 
         color: '#001926', 
         height: 'clamp(2rem, 8%, 15%)', 
-        //push marquee to the very bottom 
-        position: 'absolute',
-        bottom: '0',  
+        //push marquee to the very bottom  
         letterSpacing: '.3rem', 
     }
 
     return (
         <Marquee 
+        className='customMarquee'
         style={marqueeStyle} 
         gradient={false}
         pauseOnHover={true}
